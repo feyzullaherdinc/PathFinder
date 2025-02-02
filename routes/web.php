d13d2a5a7385112bbef8808e7ab7999737ff6039
Feyzullah Erdinc <erdincfeyzullah7@gmail.com>
2025-02-02 23:58:55 +0300

    git commit -m "feat(map): add location display with Leaflet.js"

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocationController;
 
+
 Route::get('/', function () {
     return "<b> Welcome, PathFinder API V1";
 });
-
+Route::get('/maps/{id}',[LocationController::class, 'maps']);
 
